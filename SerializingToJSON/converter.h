8966a98df5422184719c56821bb480158593397d
unknown <ilker199991@gmail.com>
2022-09-19 12:32:12 +0300

    Added converter class and complete the program

--- SerializingToJSON/converter.h
+++ SerializingToJSON/converter.h
@@ -2,12 +2,21 @@
 #define CONVERTER_H
 
 #include <QObject>
-
+#include "test.h"
+#include <QVariant>
+#include <QFile>
+#include <QVariantMap>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QMap>
 class Converter : public QObject
 {
     Q_OBJECT
 public:
     explicit Converter(QObject *parent = nullptr);
+
+    static void writeJson(Test *obj,QString path);
+    static Test* readJson(QString path);
 
 signals:
 
